c72997ee8c833bcb2b9b78dd76c12278721e06bb
Daniel Klaus <danielklaus@hotmail.de>
2018-02-28 15:25:33 +0100

    Datenbank Connector fertiggestellt. Beispiel für die Benutzung in Klasse CreateUserTest

--- Maven/src/jdbc/CreateUserTest.java
+++ Maven/src/jdbc/CreateUserTest.java
@@ -1,9 +1,16 @@
 package jdbc;
 
+import java.io.IOException;
+
 public class CreateUserTest {
 	public static void main(String[] args) {
 		jdbcConnector conn = new jdbcConnector();
-		String queryString = "INSERT INTO `USER`(`Username`, `Password`) VALUES (`Cooler Nutzername`, `Passwort-Hash`)";
-		conn.query(queryString);
+		String queryString = "INSERT INTO `USER`(`Username`, `Password`) VALUES (\"Test-User\", \"Passwort-Hash\")";
+		try {
+			conn.query(queryString);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
