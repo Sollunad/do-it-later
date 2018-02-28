353a3d514c0807afa874ac09eda4045254ca1763
Daniel Klaus <danielklaus@hotmail.de>
2018-02-28 15:40:54 +0100

    Connector angepasst, gibt jetzt JSON-String zurück statt nur in der Konsole

--- Maven/src/jdbc/CreateUserTest.java
+++ Maven/src/jdbc/CreateUserTest.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 public class CreateUserTest {
 	public static void main(String[] args) {
 		jdbcConnector conn = new jdbcConnector();
-		String queryString = "INSERT INTO `USER`(`Username`, `Password`) VALUES (\"Test-User\", \"Passwort-Hash\")";
+		//String queryString = "INSERT INTO `USER`(`Username`, `Password`) VALUES (\"Test-User\", \"Passwort-Hash\")";
+		String queryString = "SELECT * FROM `USER`";
 		try {
-			conn.query(queryString);
+			String jsonReturn = conn.query(queryString);
+			System.out.println(jsonReturn);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- Maven/src/jdbc/jdbcConnector.java
+++ Maven/src/jdbc/jdbcConnector.java
@@ -12,19 +12,19 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class jdbcConnector {
-	   // JDBC driver name and database URL
+	   // JDBC driver name and database URL (plaintext rules)
 	
 	   static final String DB_NAME = "DB3271372";
 	   static final String DB_USER = "U3271372";
 	   static final String DB_PASS = "Tofuwurst42";   
 	   
-	   public void query(String queryString) throws IOException {
+	   public String query(String queryString) throws IOException {
 	        //Body des POST-Requests, der an das PHP-Skript geschickt werden soll
 	        String body = "db=" + URLEncoder.encode(DB_NAME, "UTF-8") + "&"
 	                + "user=" + URLEncoder.encode(DB_USER, "UTF-8") + "&"
 	                + "pass=" + URLEncoder.encode(DB_PASS, "UTF-8") + "&"
 	                + "query=" + URLEncoder.encode(queryString, "UTF-8");
-	 
+	        	 
 	        //HTTP-Verbindung mit Webserver vorbereiten
 	        URL url = new URL("http://sollunad.de/do-it-later/database_api.php");
 	        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -35,17 +35,20 @@ public class jdbcConnector {
 	        connection.setRequestProperty("Content-Type",
 	                "application/x-www-form-urlencoded");
 	        connection.setRequestProperty("Content-Length", String.valueOf(body.length()));
-	         
+	        
+	        String outputString = ""; 
 	        //Request an Webserver senden und Ergebnis ausgeben
 	        try (Writer writer = new OutputStreamWriter(connection.getOutputStream())) {
 	            writer.write(body);
 	            writer.flush();
 	            try (Scanner scanner = new Scanner(connection.getInputStream())) {
 	                while (scanner.hasNextLine()) {
-	                    System.out.println(scanner.nextLine());
+	                	String nextLine = scanner.nextLine();
+	                    outputString = outputString + nextLine;
 	                }
 	            }
 	        }
+	        return outputString;
 	   }
 }
 
