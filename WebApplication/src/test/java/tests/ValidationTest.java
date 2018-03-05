5a1dcb768f3737c7227b4201c913a3ddb7532856
Stefan Reschke <stefan.reschke@outlook.de>
2018-03-05 19:03:53 +0100

    Minor fixes

---
+++ WebApplication/src/test/java/tests/ValidationTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import resources.Validation;
+
+class ValidationTest {
+
+	@Test
+	void registerPasswordHardness() {
+		List<String> correct = new ArrayList<>();
+		List<String> incorrect = new ArrayList<>();
+		correct.add("aAbB66@qQq");
+		incorrect.add("aAbB66aqQq");
+		incorrect.add("aAbBeeaqQq");
+		incorrect.add("aAbBee");
+		for (String pw : correct)
+			assertTrue(Validation.checkPasswordForHardness(pw), "Richtiges Passwort wurde als falsch bezeichnet!");
+		for (String pw : incorrect)
+			assertFalse(Validation.checkPasswordForHardness(pw), "Falsches Passwort wurde als richtig bezeichnet!");
+	}
+
+}
